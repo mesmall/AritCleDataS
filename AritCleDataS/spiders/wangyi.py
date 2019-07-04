0dd8b8e1ad25a0250995cc634a91d00701952cec
lizhen <2225213990@qq.com>
2019-07-04 14:59:31 +0800

    wangyi

---
+++ AritCleDataS/spiders/wangyi.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+import scrapy
+
+
+class WangyiSpider(scrapy.Spider):
+
+    name = 'wangyi'
+
+    def start_requests(self):
+        for url in urls:
+    # allowed_domains = ['http://c.m.163.com/nc/auto/list/5bmz6aG25bGx/0-20.html']
+    # start_urls = ['http://http://c.m.163.com/nc/auto/list/5bmz6aG25bGx/0-20.html/']
+
+
+
+    def parse(self, response):
+
+        pass
